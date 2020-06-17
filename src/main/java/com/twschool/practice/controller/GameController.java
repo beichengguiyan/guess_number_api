4cbdbd70f2aa2437f24a13fd9d821a49b43c05f2
beichengguiyan <chihaotian@zybank.com.cn>
2020-06-17 22:57:35 +0800

    modify gamecontroller

--- src/main/java/com/twschool/practice/controller/GameController.java
+++ src/main/java/com/twschool/practice/controller/GameController.java
@@ -1,22 +1,44 @@
 package com.twschool.practice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.twschool.practice.model.UserGameInfo;
+import com.twschool.practice.model.UserGameInfoDTO;
+import com.twschool.practice.model.UserGameResponse;
+import com.twschool.practice.repository.UserGameRepository;
+import com.twschool.practice.service.GuessNumberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class GameController {
+    @Autowired
+    GuessNumberService guessNumberService;
 
-    @GetMapping("/game")
-    public Map<String, String> guess(@RequestParam String guess) {
-        String result;
-        result ="4A0B";
-        Map<String, String> map = new HashMap<>();
-        map.put("input", guess);
-        map.put("result", result);
-        return map;
+    @Autowired
+    UserGameRepository userGameRepository;
+    @PostMapping("/check")
+    public UserGameResponse check(@RequestBody UserGameInfoDTO userGameInfoDTO){
+
+        UserGameInfo userGameInfo = userGameRepository.getUserGameInfoById(userGameInfoDTO.getUserId());
+
+        if (userGameInfo == null){
+            UserGameInfo registerUserGameInfo = new UserGameInfo();
+            registerUserGameInfo.setAnswer(null);
+            registerUserGameInfo.setCount(0);
+            registerUserGameInfo.setIntegral(0);
+            registerUserGameInfo.setUserId(userGameInfoDTO.getUserId());
+            registerUserGameInfo.setExtraIntegral(0);
+            registerUserGameInfo.setContinuousRightCount(0);
+
+            userGameRepository.registerUserGameInfo(registerUserGameInfo);
+
+            userGameInfo = registerUserGameInfo;
+        }
+
+        return guessNumberService.playGame(userGameInfoDTO.getGuess(), userGameInfo);
+
     }
+
 }
