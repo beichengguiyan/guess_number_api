dfadf2499681b28bad7c181c5affeae4f4aa78f9
beichengguiyan <chihaotian@zybank.com.cn>
2020-06-17 22:42:04 +0800

    create model and repository

---
+++ src/main/java/com/twschool/practice/model/UserGameInfoDTO.java
@@ -0,0 +1,31 @@
+package com.twschool.practice.model;
+
+
+public class UserGameInfoDTO {
+    /**
+     * 用户id
+     */
+    String userId;
+
+    /**
+     * 用户输入答案
+     */
+    String guess;
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getGuess() {
+        return guess;
+    }
+
+    public void setGuess(String guess) {
+        this.guess = guess;
+    }
+}
